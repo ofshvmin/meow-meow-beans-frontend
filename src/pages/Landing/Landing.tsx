6bdb95082f2097041e7239e261777c65140fecd0
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-05-31 17:17:47 -0400

    initial commit

---
+++ src/pages/Landing/Landing.tsx
@@ -0,0 +1,25 @@
+// assets
+import logo from '../../assets/logo.svg'
+
+// css
+import styles from './Landing.module.css'
+
+// types
+import { User } from '../../types/models'
+
+interface LandingProps {
+  user: User | null;
+}
+
+const Landing = (props: LandingProps): JSX.Element => {
+  const { user } = props
+
+  return (
+    <main className={styles.container}>
+      <h1>Meow Meow <br/> Beans</h1>
+      <img src={logo} alt="A meow meow bean" />
+    </main>
+  )
+}
+
+export default Landing

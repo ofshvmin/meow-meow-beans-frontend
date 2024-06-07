f10567bc9720f9f5d0051df471db3a21c15cc053
Daniel Mathews <daniel.g.mathews@gmail.com>
2024-06-07 14:37:22 -0400

    refactor VoteManager component to render profile rating

--- src/components/VoteManager/VoteManager.tsx
+++ src/components/VoteManager/VoteManager.tsx
@@ -1,6 +1,6 @@
 //assets
 import bean from '../../assets/icons/bean.png'
-import nobean from '../../assets/icons/noBean.png'
+import noBean from '../../assets/icons/noBean.png'
 
 //types
 import { Profile } from "../../types/models"
@@ -13,6 +13,15 @@ const voteManager = (props: voteManagerProps): JSX.Element => {
   const { profile } = props
 
   const ratingOptions = [1, 2, 3, 4, 5]
+  const voteCount = profile.votesReceived.length
+  const voteSum: number = profile.votesReceived.reduce(
+    (sum: number, v: { value: number }): number => {
+      return sum + v.value
+    },
+    0
+  )
+
+  const profileRating = voteCount ? voteSum / voteCount : 1
 
   return (
     <section>
@@ -20,7 +29,7 @@ const voteManager = (props: voteManagerProps): JSX.Element => {
         <img
           id={rating.toString()} 
           key={rating}
-          src={nobean}
+          src={rating <= profileRating ? bean : noBean}
           alt="Bean Symbol"
         />
       ))}
